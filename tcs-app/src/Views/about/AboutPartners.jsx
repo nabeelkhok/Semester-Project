9c13deabe2ffec662efcfc443c7468a6a5b2af47
nabeelkhok <161445085+nabeelkhok@users.noreply.github.com>
2025-06-03 01:26:15 +0500

    Update AboutPartners.jsx

--- tcs-app/src/Views/about/AboutPartners.jsx
+++ tcs-app/src/Views/about/AboutPartners.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 function AboutPartners() {
-    return (
-        <section className="container mx-auto text-center my-10">
-            <h2 className="text-2xl font-semibold">Our Trusted Partners</h2>
-            <div className="flex justify-center mt-6 space-x-4">
-                <img src="../../assets/image.png" alt="Partner 1" className="h-12" />
-                <img src="../../assets/image.png" alt="Partner 2" className="h-12" />
-            </div>
-        </section>
-    );
+  return (
+    <section className="w-full bg-white py-12">
+      <div className="max-w-4xl mx-auto px-4 text-center">
+        <h2 className="text-3xl font-semibold text-gray-800">Our Trusted Partners</h2>
+        
+        <div className="flex flex-wrap justify-center items-center mt-8 gap-6">
+          <img src={Partner1} alt="Partner 1" className="h-12 object-contain" />
+          <img src={Partner2} alt="Partner 2" className="h-12 object-contain" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutPartners;
